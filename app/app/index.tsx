7a5489294d7f8ddaacd697ab0813465b7c520422
Jolan <jolan.callens@gmail.com>
2025-02-10 15:36:15 -1000

    Put dummy Drawer content

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,5 +1,6 @@
 import AppLogo from "@/components/AppLogo";
-import CustomDrawer from "@/components/CustomDrawer";
+import CustomDrawer from "@/components/drawer/CustomDrawer";
+import CustomDrawerContent from "@/components/drawer/CustomDrawerContent";
 import { Link } from "expo-router";
 import { useState } from "react";
 import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
@@ -11,24 +12,22 @@ export default function Index() {
     return (
         <View style={styles.container}>
 
-        <CustomDrawer 
-            isVisible={isDrawerVisible} 
-            onClose={() => setIsDrawerVisible(false)}
-        >
-            <SafeAreaView>
-                <Text>Contenu du drawer</Text>
+            <CustomDrawer 
+                isVisible={isDrawerVisible} 
+                onClose={() => setIsDrawerVisible(false)}
+            >
+                <CustomDrawerContent />
+            </CustomDrawer>
+
+            <SafeAreaView style={styles.contentContainer}>
+                <AppLogo onPress={() => setIsDrawerVisible(true)} />
             </SafeAreaView>
-        </CustomDrawer>
 
-        <SafeAreaView style={styles.contentContainer}>
-            <AppLogo onPress={() => setIsDrawerVisible(true)} />
-        </SafeAreaView>
-
-        {/* <Link href={{ pathname: '/article/[id]', params: {id: 1} }} asChild>
-            <Pressable>
-            <Text>Go to article 1</Text>
-            </Pressable>
-        </Link> */}
+            {/* <Link href={{ pathname: '/article/[id]', params: {id: 1} }} asChild>
+                <Pressable>
+                <Text>Go to article 1</Text>
+                </Pressable>
+            </Link> */}
 
         </View>
     );
---
+++ app/components/drawer/CustomDrawerContent.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function CustomDrawerContent() {
+    
+    return (
+        <SafeAreaView>
+            <Text>Contenu du drawer</Text>
+        </SafeAreaView>
+    )
+}
